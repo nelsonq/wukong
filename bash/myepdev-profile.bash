53d9d0d781ca723b701e7fbe2b008774f93b9cd5
Hua(Nelson) Qiao <nelson.qiao@elasticpath.com>
2019-12-20 13:45:08 -0800

    [WUKONG-13] added support to 7.6

--- bash/myepdev-profile.bash
+++ bash/myepdev-profile.bash
@@ -2,18 +2,23 @@
 export MAVEN_INST_ROOT=${MAVEN_INST_ROOT:=~/apphome}
 
 # M2_HOME configuration
+# [check point] In case of maven got upgrade.
+export M2_HOME_362=$MAVEN_INST_ROOT/apache-maven-3.6.2
 export M2_HOME_354=$MAVEN_INST_ROOT/apache-maven-3.5.4
 export M2_HOME_352=$MAVEN_INST_ROOT/apache-maven-3.5.2
 export M2_HOME_339=$MAVEN_INST_ROOT/apache-maven-3.3.9
 export M2_HOME_305=$MAVEN_INST_ROOT/apache-maven-3.0.5
 
 # MAVEN_OPTS configuration
+# [check point] In case of maven option got upgrade.
 export MAVEN_OPTS_305='-Xmx1024m -XX:MaxPermSize=512m -XX:ReservedCodeCacheSize=128m -Dsun.lang.ClassLoader.allowArraySyntax=true -ea'
 export MAVEN_OPTS_339='-Xmx2048m -XX:ReservedCodeCacheSize=128m -Dsun.lang.ClassLoader.allowArraySyntax=true'
 export MAVEN_OPTS_352=$MAVEN_OPTS_339
 export MAVEN_OPTS_354=$MAVEN_OPTS_352
+export MAVEN_OPTS_362=$MAVEN_OPTS_354
 
 # JAVA_HOME configuration
+# [check point] In case of jdk got upgrade.
 os="$(uname -s)"
 if [ "$os" = "Darwin" ];then
 export JAVA_HOME_8U66=/Library/Java/JavaVirtualMachines/jdk1.8.0_66.jdk/Contents/Home/
@@ -45,6 +50,7 @@ export JAVA_OPTS_7='-Xmx1024m -XX:MaxPermSize=512m -Dsun.lang.ClassLoader.allowA
 # ==============================
 
 # EP release mapping of each configuration
+# [check point] In case of ep commerce got new release.
 # suppose ep 614 should use 7u51, but mac os doesn't allow to install unless some hacking. use 7u79 instead.
 export JAVA_HOME_EP614=$JAVA_HOME_7U79;export M2_HOME_EP614=$M2_HOME_305;export JAVA_OPTS_EP614=$JAVA_OPTS_7;export MAVEN_OPTS_EP614=$MAVEN_OPTS_305
 export JAVA_HOME_EP6152=$JAVA_HOME_8U66;export M2_HOME_EP6152=$M2_HOME_305;export JAVA_OPTS_EP6152=$JAVA_OPTS_8;export MAVEN_OPTS_EP6152=$MAVEN_OPTS_305
@@ -56,7 +62,10 @@ export JAVA_HOME_EP72=$JAVA_HOME_8U144;export M2_HOME_EP72=$M2_HOME_339;export J
 export JAVA_HOME_EP73=$JAVA_HOME_8U144;export M2_HOME_EP73=$M2_HOME_352;export JAVA_OPTS_EP73=$JAVA_OPTS_8;export MAVEN_OPTS_EP73=$MAVEN_OPTS_352
 export JAVA_HOME_EP74=$JAVA_HOME_8U144;export M2_HOME_EP74=$M2_HOME_352;export JAVA_OPTS_EP74=$JAVA_OPTS_8;export MAVEN_OPTS_EP74=$MAVEN_OPTS_352
 export JAVA_HOME_EP75=$JAVA_HOME_8U212;export M2_HOME_EP75=$M2_HOME_352;export JAVA_OPTS_EP75=$JAVA_OPTS_8;export MAVEN_OPTS_EP75=$MAVEN_OPTS_352
-export JAVA_HOME_EP7x=$JAVA_HOME_8;export M2_HOME_EP7x=$M2_HOME_354;export JAVA_OPTS_EP7x=$JAVA_OPTS_8;export MAVEN_OPTS_EP7x=$MAVEN_OPTS_352
+export JAVA_HOME_EP76=$JAVA_HOME_8U212;export M2_HOME_EP76=$M2_HOME_362;export JAVA_OPTS_EP76=$JAVA_OPTS_8;export MAVEN_OPTS_EP76=$MAVEN_OPTS_362
+
+# [check point] In case of ep commerce got new release. 
+export JAVA_HOME_EP7x=$JAVA_HOME_EP76;export M2_HOME_EP7x=$M2_HOME_EP76;export JAVA_OPTS_EP7x=$JAVA_OPTS_EP76;export MAVEN_OPTS_EP7x=$MAVEN_OPTS_EP76
 
 # export default configuration
 export JAVA_HOME=$JAVA_HOME_EP7x
